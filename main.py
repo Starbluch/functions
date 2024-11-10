314d8d24bfc189a4c9766b425a6a0dc199d040d3
admin <gadzukoleksandr29@gmail.com>
2024-11-10 20:02:18 +0200

    ex_1 from hm is done

---
+++ main.py
@@ -0,0 +1,13 @@
+def print_text():
+
+    RED = "\033[91m"
+    Black ="\033[30m"
+    RESET = "\033[0m"
+
+
+    print(f"{Black}\" Don't compare yourself with anyone in this world...{RESET}")
+    print(f"{RED}if{RESET} you {RED} do so{RESET}, you are insulting yourself.\"")
+    print("Bill Gates")
+
+
+print_text()
